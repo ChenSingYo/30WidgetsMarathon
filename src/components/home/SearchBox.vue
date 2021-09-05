913c25456dfbdd8979451161b570952afe599c0e
ChenSingYo <chensingyo@gmail.com>
2021-09-06 00:41:01 +0800

    fix: Search Box Effect for iPhone

--- src/components/home/SearchBox.vue
+++ src/components/home/SearchBox.vue
@@ -72,4 +72,28 @@ export default {
     padding: 0.5rem;
   }
 }
+
+/* smartphones, touchscreens */
+@media (pointer: coarse) and (any-pointer: fine) {
+		.btn-search:hover ~ .input-search,
+		.btn-search:active ~ .input-search {
+		width: 180px;
+		background-color: #fff;
+    border: 3px solid gray;
+    padding: 0.5rem;
+		transition: all 300ms cubic-bezier(0,-0.23,.35,2);
+	}
+		.btn-search:hover ~ .input-search::placeholder,
+		.btn-search:active ~ .input-search::placeholder {
+    color: grey;
+		font-weight:200;
+  }
+		.input-search:hover,
+		.input-search:active {
+    border: 3px solid black;
+    width: 180px;
+    padding: 0.5rem;
+  }
+}
+
 </style>
