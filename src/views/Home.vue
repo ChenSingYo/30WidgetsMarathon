fcd8004d5bf060b998261b5a1d4acbd268c47c40
ChenSingYo <chensingyo@gmail.com>
2021-09-09 10:43:43 +0800

    update card info: home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
 <!-- RWD Grid Layout -->
 <div class="grid gap-4 max-w-7xl xs:grid-cols-2 xs:gap-6 md:grid-cols-4 md:gap-4 lg:gap-6">
-    <header class="h-auto flex flex-col xs:col-span-2 mb-3">
+    <header class="h-auto mt-10 flex flex-col xs:col-span-2 mb-3">
       <h2 class="text-4xl text-black font-extrabold p-4 pt-1">30 Widgets Marathon</h2>
       <h5 class="pl-4 text-[1rem] text-gray-700 font-normal">2021 鐵人賽專案：30天30個widgets</h5>
     </header>
 
-    <div class="xs:col-span-2 flex xs:justify-start md:justify-end">
+    <div class="relative xs:col-span-2 mt-10 flex xs:justify-start md:justify-end">
+      <div class="absolute top-[-15%] right-0 font-bold text-shadow-sm">#1</div>
       <search-box/>
     </div>
 
@@ -34,73 +35,78 @@
 
    <router-link to="/4">
     <div class="card h-[100px] bg-yellow-500 hover:bg-yellow-400 xs:h-auto xs:square">
-      <div class="font-bold text-shadow">#5</div>
-      <p class="font-bold text-shadow">Q&A Section</p>
+      <div class="font-bold text-shadow-sm">#5</div>
+      <p class="font-bold text-shadow-sm">Q&A Section</p>
     </div>
      </router-link>
 
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <div class="font-bold text-shadow-sm">#6</div>
+      <p class="font-bold text-shadow-sm">Scroll Animation</p>
     </div>
 
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <div class="font-bold text-shadow-sm">#7</div>
+      <p class="font-bold text-shadow-sm">Progress Steps</p>
     </div>
 
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <div class="font-bold text-shadow-sm">#8</div>
+      <p class="font-bold text-shadow-sm">Button Ripple Effect</p>
     </div>
     
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <div class="font-bold text-shadow-sm">#9</div>
+      <p class="font-bold text-shadow-sm">Netfilx Navigation</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <div class="font-bold text-shadow-sm">#10</div>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
         <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-yellow-400 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
     <div class="card h-[100px] bg-gray-600 hover:bg-green-500 xs:h-auto xs:square">
-      <p class="font-bold text-shadow">Preparing...</p>
+      <p class="font-bold text-shadow-sm">Preparing...</p>
     </div>
 </div>
 
@@ -115,7 +121,6 @@
   align-items: center;
   color: white;
   font-size: 15px;
-  // cursor: pointer;
 }
 
 .text-shadow {
